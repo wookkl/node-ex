f4ff75fd34025da9dda05a5bad28700729b2aaac
wookkl <wjddnr315@gmail.com>
2022-02-21 23:54:23 +0900

    Add db connection middleware

---
+++ node-ex/src/middleware.ts
@@ -0,0 +1,30 @@
+import * as express from 'express';
+import { type } from 'os';
+import * as typeORM from 'typeorm';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+const getOrCreateConnection = async (args: PostgresConnectionOptions): Promise<void> => {
+  try {
+    await typeORM.createConnection(args);
+  } catch (error) {
+    await typeORM.getConnection(args);
+  }
+};
+
+export const dbConnectionMiddleware = async(request: express.Request, callback: express.Response, next) => {
+  try {
+    await getOrCreateConnection({
+      name: 'defaut',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      type: 'postgres',
+      synchronize: false,
+      
+    });
+    next();
+  } catch (error){
+    return await callback.status(500).json({message: 'DB connection failed', error})
+  }
+}
