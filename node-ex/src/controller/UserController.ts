751a0af81c0f6102c41eac9d656353821e132ed2
wookkl <wjddnr315@gmail.com>
2022-03-19 14:27:45 +0900

    Add user controller

--- node-ex/src/controller/UserController.ts
+++ node-ex/src/controller/UserController.ts
@@ -1,10 +1,28 @@
-import {getRepository} from "typeorm";
 import {NextFunction, Request, Response} from "express";
 import {User} from "../entity/account/User";
+import {UserRepository} from "../repository/UserRepository";
+import {DeepPartial} from "typeorm";
 
 export class UserController {
+    constructor() {
+    }
 
-    private userRepository = getRepository(User);
+    private userRepository = new UserRepository();
 
+    async getById(request: Request, response: Response, next: NextFunction): Promise<User> {
+        const id = request.params.structureId;
+        return await this.userRepository.findOne(id);
+    }
 
+    create(request: Request, response: Response, next: NextFunction): User {
+        const validatedData = UserController.validateUserCreateData(request.body);
+        return this.userRepository.create(validatedData);
+    }
+
+    private static validateUserCreateData(data: Record<string, unknown>): DeepPartial<User> {
+        return {
+            username: data.username as string,
+            password: data.password as string,
+        };
+    }
 }
