19c5c21371af7a641a35578c7ced1e1e83a6fab7
wookkl <wjddnr315@gmail.com>
2022-03-27 16:18:56 +0900

    Extend user repository

--- node-ex/src/repository/UserRepository.ts
+++ node-ex/src/repository/UserRepository.ts
@@ -1,8 +1,27 @@
 import {EntityRepository, Repository} from "typeorm";
 import { User } from "../entity/account/User"
 
-
+export interface UserPaginationResponse {
+    data: User[];
+    totalPage: number;
+}
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
+    async findByTerm(term?: string, page?: number): Promise<UserPaginationResponse> {
+        let queryBuilder = this.createQueryBuilder("user")
+            .innerJoin("user.profile", "profile")
+            .take(10);
+        if (term) {
+            queryBuilder = queryBuilder.where(
+                "user.username ILIKE :term OR profile.email ILIKE :term OR profile.nickname ILIKE :term",
+            {term: `%${term}%`}
+            );
+        }
+        if (page) {
+            queryBuilder = queryBuilder.skip(10 * Number(page - 1));
+        }
+        const [data, count] = await queryBuilder.getManyAndCount();
+        return { data, totalPage: Math.ceil(count / 10)};
+    }
 
 }
